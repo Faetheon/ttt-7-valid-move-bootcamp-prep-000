4012e7e2d6433f8abd35e73705f176d077aeb26f
Nathan Hill <nateohill@gmail.com>
2018-05-23 16:18:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(arr, index)
-    if arr[index] === "" || arr[index] === " " || arr[index] === nil || arr[].length > index
+    if arr[index] === "" || arr[index] === " " || arr[index] === nil || arr.length < index
     return false
   else
     return true
